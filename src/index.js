080308b634fdb71cac201864987cf3dc8ec600b0
amymc <amy.mccarthy2@mail.dcu.ie>
2017-08-10 22:47:59 +0100

    dot.env setup

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv/config'
 import Mail from './models/mail';
-
-require('dotenv').config();
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -16,10 +15,22 @@ const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect(`mongodb://${process.env.USER}:${process.env.PASSWORD}@${process.env.host}/hate-mail`);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+mongoose.connect(`mongodb://${process.env.USER_NAME}:${process.env.PASSWORD}@${process.env.HOST}/hate-mail`);
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
+  next();
+});
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
